d4616b0c68f200d1148e100f0e989e0f90df618a
Jon Zelner <jzelner@gmail.com>
2022-04-11 11:10:44 -0400

    Updates to spatial clustering interactive
    Did some cleanup and also added Theil index based clustering metric.

--- segregation-measures/src/server.R
+++ segregation-measures/src/server.R
@@ -1,4 +1,5 @@
 require(shiny)
+require(flexdashboard)
 #source("src/segregation.R")
 
 ## Generate grid
@@ -23,16 +24,28 @@ isoval <- reactive({
   isolation(neighborhood_df()$p*1000, 1000)
 })
 
+interactval <- reactive({
+  interaction(neighborhood_df()$p*1000, 1000)
+})
 
+theilval <- reactive({
+  ldf <- neighborhood_df_long()
+  x <- 100*(theil(ldf, "quadrant") + theil_within(ldf, "quadrant", "block"))
+  print(as.numeric(x))
+  return(as.numeric(x))
+})
 
 dissimval <- reactive({
   dissimilarity(neighborhood_df()$p*1000, 1000)
 })
 
 moranval <- reactive({
-  morans_grid(d, neighborhood_df()$p)$I
+  morans_grid(d, input$ntype, neighborhood_df()$p)$I
 })
 
+unvaxval <- reactive({
+  round(mean(100*neighborhood_df()$p),0)
+})
 
 output$avgp <- renderText(
   paste0("Percent unvaccinated = ", round(mean(100*neighborhood_df()$p),2),"%")
@@ -53,30 +66,84 @@ output$morans <- renderText(
 
 output$blocktheil <- renderText({
   ldf <- neighborhood_df_long()
-  block_h <- theil(ldf, "block")
-  paste0("Block:", round(block_h,2))
+  block_h <- theil(ldf, "quadrant") + theil_within(ldf, "quadrant", "block")
+  paste0("Total:", round(block_h,2))
 }
 )
 
 output$neightheil <- renderText({
   ldf <- neighborhood_df_long()
   block_h <- theil(ldf, "neighborhood")
-  paste0("Neighborhood:", round(block_h,2))
+  paste0("Between Neighborhood:", round(block_h,2))
 }
 )
 
 output$quadtheil <- renderText({
   ldf <- neighborhood_df_long()
-  block_h <- theil(ldf, "quadrant")
-  paste0("Quadrant:", round(block_h,2))
+  block_h <- 100*theil(ldf, "quadrant") / (theil(ldf, "quadrant") + theil_within(ldf, "quadrant", "block"))
+  paste0("Between Quadrant:", round(block_h,0),"%")
+}
+)
+
+output$withinquadtheil <- renderText({
+  ldf <- neighborhood_df_long()
+  block_h <- 100*theil_within(ldf, "quadrant", "block") / (theil(ldf, "quadrant") + theil_within(ldf, "quadrant", "block"))
+  paste0("Within quadrant:", round(block_h,0),"%")
 }
 )
 
 output$halftheil <- renderText({
   ldf <- neighborhood_df_long()
   block_h <- theil(ldf, "high_risk_h")
-  paste0("Half:", round(block_h,2))
+  paste0("Between Halves:", round(block_h,2))
 }
 )
+
+output$theilgauge <- renderGauge({
+  gauge(round(theilval(),2),
+        min = 0,
+        max = 100)
+})
+
+output$dissimgauge <- renderGauge({
+
+  
+  gauge(round(dissimval(),2),
+        min = 0,
+        max = 1)
+  
+})
+
+output$isogauge <- renderGauge({
+  
+  gauge(round(isoval(),2),
+        min = 0,
+        max = 1)
+  
+})
+
+output$intergauge <- renderGauge({
+  
+  gauge(round(interactval(),2),
+        min = 0,
+        max = 1)
+  
+})
+
+
+
+output$morangauge <- renderGauge({
+  
+  gauge(round(moranval(),2),
+        min = -1,
+        max = 1)
+  
+})
+
+output$unvaxgauge <- renderGauge({
+  gauge(unvaxval(),
+        min = 0, 
+        max = 100)
+})
 
 
